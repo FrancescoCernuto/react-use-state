6e6368d283c259d73ec626225ddb293cfb5ba61f
FrancescoCernuto <cernutofrancesco2@gmail.com>
2024-12-05 00:28:11 +0100

    add filter

--- vite-project/src/assets/components/Tag/TagList.jsx
+++ vite-project/src/assets/components/Tag/TagList.jsx
@@ -1,1 +1,16 @@
 import Tag from "./Tag";
+
+export default function TagList({ tags, handleTagClick }) {
+    return (
+        <section className="filtering-tags">
+            {tags.map((tag, index) => (
+                <Tag
+                    key={index}
+                    index={index}
+                    tag={tag}
+                    handleTagClick={handleTagClick}
+                />
+            ))}
+        </section>
+    );
+}
