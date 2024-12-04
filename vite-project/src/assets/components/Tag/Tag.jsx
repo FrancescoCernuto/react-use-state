43c2307f9549caf1cad680f95c660c5505607324
FrancescoCernuto <cernutofrancesco2@gmail.com>
2024-12-05 00:09:35 +0100

    export tag + return button

--- vite-project/src/assets/components/Tag/Tag.jsx
+++ vite-project/src/assets/components/Tag/Tag.jsx
@@ -1,1 +1,12 @@
-export default function Tag({ index, tag, handleTagClick }) { }
+export default function Tag({ index, tag, handleTagClick }) {
+    return (
+        <>
+            <button
+                className={`tag ${tag.status}`}
+                onClick={() => handleTagClick(index)}
+            >
+                {tag.title}
+            </button>
+        </>
+    );
+}
